e2c9fd07a8e2361f82e89df0b0556a0fba875c76
Basit Arigbabuwo <arigbabuwobasitkorede6@gmail.com>
2022-03-30 10:50:04 +0100

    Fix linter error

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import './style.css'
+import './style.css';
 
 const toDoItems = [
   { description: 'Do the chores', completed: false, index: 0 },
-  { description: "Take a nap", completed: false, index: 1 },
+  { description: 'Take a nap', completed: false, index: 1 },
   { description: 'Do Microverse Work', completed: false, index: 2 },
-]
-const listWrap = document.querySelector('.todo-list-wrap')
+];
+const listWrap = document.querySelector('.todo-list-wrap');
 // IMPLEMENT TO DO ITEMS TO SCREEN
 const implementToDoItems = () => {
   toDoItems.forEach((task) => {
@@ -15,7 +15,7 @@ const implementToDoItems = () => {
         <input class="description-input" type="text" value="${task.description}">
       </div>
       <button data-key="${task.index}" class="move-item"><i class="fas fa-ellipsis-v"></i></button>
-    </li>`
-  })
-}
-implementToDoItems()
+    </li>`;
+  });
+};
+implementToDoItems();
